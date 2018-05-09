df28ee30498b2547620e7c9e2ca22fec534705ff
tylergaugler16 <tylergaugler16@gmail.com>
2018-05-09 12:19:29 -0400

    significant improvements to overall design

--- js/script.js
+++ js/script.js
@@ -24,8 +24,97 @@ var mainBlockVisible = false;
   //   });
   //   }
   // });
-  $('#menu-button').click(function(){
-  		$('.nav').css('background-color', '#cccccc');
+  $(document).ready(function(){
+
+    if(typeof AOS != 'undefined'){
+      AOS.init({
+       duration: 500,
+     });
+    }
+    var fixmeTop  = $('div.main-navbar').offset().top;
+    $(document).on('scroll', function(){
+       var currentScroll = $(window).scrollTop();
+      $target = $('div.main-navbar').prev();
+       if (currentScroll >= fixmeTop) {
+         console.log("below");
+         $('div.main-navbar').addClass('sticky-navbar');
+         $target.css('margin-bottom', '75px');
+       }
+       else{
+         $('div.main-navbar').removeClass('sticky-navbar');
+         $target.css('margin-bottom', '0px');
+
+       }
+    })
+  })
+
+  $(document).on('mouseover','.background-pic-1', function(){
+    $('#about-us-link-text').css({ 'border-bottom': '3px solid #B3E9C7',
+                                  'font-weight': '900',
+                                  'font-size': '34px'
+                                });
+
+    $('#about-us-link-text').animate({left: '24%'}, {easing: "swing", duration: 1000});
+
+  })
+  .on('mouseleave','.background-pic-1', function(){
+      $('#about-us-link-text').clearQueue().stop();
+    $('#about-us-link-text').css({ 'border-bottom': '3px solid #27aae3',
+                                  'font-weight': '500',
+                                  'font-size': '30px'
+                                });
+  $('#about-us-link-text').animate({left: '50%'}, {easing: "swing", duration: 1000});
+  })
 
 
-  });
+  $(document).on('mouseover','.background-pic-2', function(){
+
+    $('#bible-study-link-text').css({ 'border-bottom': '3px solid #8367C7',
+                                  'font-weight': '900',
+                                  'font-size': '34px'
+                                });
+    $('#bible-study-link-text').animate({right: '30%'}, {easing: "swing", duration: 1000});
+  })
+  .on('mouseleave','.background-pic-2', function(){
+    $('#bible-study-link-text').clearQueue().stop();
+    $('#bible-study-link-text').css({ 'border-bottom': '3px solid #27aae3',
+                                  'font-weight': '500',
+                                  'font-size': '30px'
+                                });
+    $('#bible-study-link-text').animate({right: '50%'}, {easing: "swing", duration: 1000});
+  })
+
+  $(document).on('mouseover','.background-pic-3', function(){
+    $('#open-gym-link-text').css({ 'border-bottom': '3px solid #F991CC',
+                                  'font-weight': '900',
+                                  'font-size': '34px'
+                                });
+
+    $('#open-gym-link-text').animate({left: '24%'}, {easing: "swing", duration: 1000});
+
+  })
+  .on('mouseleave','.background-pic-3', function(){
+    $('#open-gym-link-text').clearQueue().stop();
+    $('#open-gym-link-text').css({ 'border-bottom': '3px solid #27aae3',
+                                  'font-weight': '500',
+                                  'font-size': '30px'
+                                });
+    $('#open-gym-link-text').animate({left: '50%'}, {easing: "swing", duration: 1000});
+  })
+
+  $(document).on('mouseover','.background-pic-4', function(){
+
+    $('#events-link-text').css({ 'border-bottom': '3px solid #FFFF82',
+                                  'font-weight': '900',
+                                  'font-size': '34px'
+                                });
+    $('#events-link-text').animate({right: '24%'}, {easing: "swing", duration: 1000});
+  })
+  .on('mouseleave','.background-pic-4', function(){
+    $('#events-link-text').clearQueue().stop();
+    $('#events-link-text').css({ 'border-bottom': '3px solid #27aae3',
+                                  'font-weight': '500',
+                                  'font-size': '30px'
+                                });
+    $('#events-link-text').animate({right: '50%'}, {easing: "swing", duration: 1000});
+  })
